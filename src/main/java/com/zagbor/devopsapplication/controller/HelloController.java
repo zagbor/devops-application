684e1ab43d5c73a8bb3d4c7a93aec429a8fca828
Boris Zagray <Boris.Zagray@x5.ru>
2024-12-02 18:59:30 +0200

    Initial commit

---
+++ src/main/java/com/zagbor/devopsapplication/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.zagbor.devopsapplication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloDocker() {
+        return "Hello, Docker with Spring Boot!";
+    }
+}
---
+++ src/test/java/com/zagbor/devopsapplication/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zagbor.devopsapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
