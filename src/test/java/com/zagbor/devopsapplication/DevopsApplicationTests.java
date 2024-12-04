6ed3f0203fc7815f030ef8b5e974b3999b93a83c
Boris Zagray <Boris.Zagray@x5.ru>
2024-12-04 17:42:36 +0200

    add tests

--- src/test/java/com/zagbor/devopsapplication/DevopsApplicationTests.java
+++ src/test/java/com/zagbor/devopsapplication/DevopsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.zagbor.devopsapplication;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class DevopsApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertEquals(1, 1);
     }
 
 }
